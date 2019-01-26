0651fc2bd5f9390dfb0d00cc5f31954fd5462d1e
antoinezimmermann <antoine.d.zimmermann@gmail.com>
2019-01-26 17:19:30 +0100

    Use custom index.html and stylesheet and reagent
    Add two new files for static assets.
    Use reagent.
    Should hot reload.

--- project.clj
+++ project.clj
@@ -4,12 +4,14 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/clojurescript "1.10.439"]]
+                 [org.clojure/clojurescript "1.10.439"]
+                 [reagent "0.8.1"]]
   :plugins [[cider/cider-nrepl "0.18.0"]]
   :profiles {:dev
              {:dependencies [[org.clojure/clojurescript "1.10.439"]
                              [com.bhauman/figwheel-main "0.2.0"]
-                             [cider/piggieback "0.3.8"]]
+                             [cider/piggieback "0.3.8"]
+                             [reagent "0.8.1"]]
               :resource-paths ["target"]
               :clean-targets ^{:protect false}["target"]
               :repl-options {:nrepl-middleware [cider.piggieback/wrap-cljs-repl]}}})
--- src/data_explorer/core.cljs
+++ src/data_explorer/core.cljs
@@ -1,4 +1,11 @@
-(ns data-explorer.core)
+(ns data-explorer.core
+  (:require [reagent.core :as r :refer [atom]]))
 
+(def app-dom-element (.getElementById js/document "app"))
 
-(println "hello world")
+(defn hello [name]
+  [:div (str "hello " name)])
+
+(r/render
+  [hello "antoine"]
+  app-dom-element)
