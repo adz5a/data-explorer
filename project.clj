50821ffd6cb8d8082feffda0b276022d690d452d
antoinezimmermann <antoine.d.zimmermann@gmail.com>
2019-01-26 16:59:10 +0100

    Initial commit
    Sets up the project.
    This project is based on figwheel.main and set up to work with
    vim-fireplace. See https://figwheel.org/docs/vim.html for docs.
    
    In order to start the CLJS repl run:
    ```
    $ lein repl # starts an nrepl
    
    user => (require 'figwheel.main.api)
    user => (figwheel.main.api/start {:mode :serve} "dev")
    user => (figwheel.main.api/repl-env "dev") ;; at this point a browser
    ;; should have been started and a page printing the doc of figwheel should
    ;; be up
    ```
    
    Then within a *.cljs buffer:
    ```
    :Piggieback (figwheel.main.api/repl-env "dev")
    ```
    
    At this point mapping such as `cpp` inside a *.cljs buffer should eval the
    form. You can still use the regular Clojure repl from within a *.clj
    buffer.

---
+++ project.clj
@@ -0,0 +1,15 @@
+(defproject data-explorer "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/clojurescript "1.10.439"]]
+  :plugins [[cider/cider-nrepl "0.18.0"]]
+  :profiles {:dev
+             {:dependencies [[org.clojure/clojurescript "1.10.439"]
+                             [com.bhauman/figwheel-main "0.2.0"]
+                             [cider/piggieback "0.3.8"]]
+              :resource-paths ["target"]
+              :clean-targets ^{:protect false}["target"]
+              :repl-options {:nrepl-middleware [cider.piggieback/wrap-cljs-repl]}}})
---
+++ src/data_explorer/core.cljs
@@ -0,0 +1,4 @@
+(ns data-explorer.core)
+
+
+(println "hello world")
